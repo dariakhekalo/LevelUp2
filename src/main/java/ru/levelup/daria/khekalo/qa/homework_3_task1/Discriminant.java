20c1eae73885bcca866ca0f29053575e306ca81f
Daria Khekalo <dariakhekalo@gmail.com>
2019-04-03 18:59:03 +0300

    homework3

---
+++ src/main/java/ru/levelup/daria/khekalo/qa/homework_3_task1/Discriminant.java
@@ -0,0 +1,47 @@
+package ru.levelup.daria.khekalo.qa.homework_3_task1;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+    public class Discriminant {
+
+
+        public static void main(String[] args) throws IOException {
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("a, b, c : ");
+            String A = reader.readLine();
+            Double a = Double.parseDouble(A);
+            String B = reader.readLine();
+            Double b = Double.parseDouble(B);
+            String C = reader.readLine();
+            Double c = Double.parseDouble(C);
+
+
+            double x1, x2;
+
+            double D = b * b - 4 * a * c;
+            if (a != 0) {
+                if (D > 0) {
+                    x1 = (-b - Math.sqrt(D)) / (2 * a);
+                    x2 = (-b + Math.sqrt(D)) / (2 * a);
+
+                    System.out.printf( "Корни уравнения: x1 = %1$.2f, x2 = %2$.2f", x1, x2);
+                } else if (D == 0) {
+                    double x;
+                    x = -b / (2 * a);
+                    System.out.printf( "Уравнение имеет единственный корень: x = %1$.2f", x);
+                } else {
+                    System.out.println("Уравнение не имеет действительных корней");
+                }
+            } else {
+                System.out.println("Это не квадратное ур-ие");
+            }
+            System.out.printf("%.2f",D);
+
+        }
+    }
+
